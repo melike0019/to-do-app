433d2c1c5e9dd28ff54a59b3be91e624c89a6c5d
melike0019 <CLDRmlk0419@gmail.com>
2025-05-28 23:53:23 +0300

    Kod açıklamaları eklendi

--- client/src/components/SortOptions.js
+++ client/src/components/SortOptions.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './SortOptions.css';
 
+// SortOptions bileşeni - Todo'ların sıralama seçeneklerini gösterir
 const SortOptions = ({ sortBy, onSortChange }) => {
   return (
     <div className="sort-options">
       <label>Sıralama:</label>
+      {/* Sıralama seçenekleri için dropdown */}
       <select
         value={sortBy}
         onChange={(e) => onSortChange(e.target.value)}
